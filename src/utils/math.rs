566a08652476f52d5c754e47ac70555251534813
CaioSimioni <caiors.contato@gmail.com>
2025-05-04 22:23:10 -0300

    Lint fix: src/utils/math.rs: help remove return

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -1,15 +1,15 @@
 // src/utils/math.rs
 
 pub fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+    a + b
 }
 
 pub fn subtract(a: i32, b: i32) -> i32 {
-    return a - b;
+    a - b
 }
 
 pub fn multiply(a: i32, b: i32) -> i32 {
-    return a * b;
+    a * b
 }
 
 pub fn divide(a: i32, b: i32) -> Result<i32, String> {
