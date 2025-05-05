80028afe1c15d2c0d5fb117dd9fed08ec48f7670
CaioSimioni <caiors.contato@gmail.com>
2025-05-04 22:06:14 -0300

    Feat: Implementação de Math
    funções matemáticas utilitárias
    add, subtract, multiply, divide

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use bash_and_blade::game;
+use bash_and_blade::utils::math;
 
 fn main() {
-    // Chama a função `show_message` do módulo `display`
-    game::display::show_message("Hello, World!");
+    println!("Inicio...");
+    println!("Soma de 2 + 3: {}", math::add(2, 3));
+    println!("Subtração de 5 - 3: {}", math::subtract(5, 3));
+    println!("Multiplicação de 2 * 3: {}", math::multiply(2, 3));
+    match math::divide(6, 3) {
+        Ok(result) => println!("Divisão de 6 / 3: {}", result),
+        Err(e) => println!("Erro: {}", e),
+    }
+    print!("Divisão de 6 / 0: ");
+    match math::divide(6, 0) {
+        Ok(result) => println!("{}", result),
+        Err(e) => println!("Erro: {}", e),
+    }
+    println!("Fim...")
 }
